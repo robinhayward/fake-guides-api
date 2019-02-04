fc84ed24782ae4a76ae0f553d2864f117effce84
Robin Hayward <mayker.invoice@gmail.com>
2019-02-04 15:09:57 +0000

    Update guides.js

--- guides.js
+++ guides.js
@@ -1,5 +1,5 @@
 [
-   { "id": "1", "name": "A Guide 1", "body": "Some useful information" },
-   { "id": "2", "name": "A Guide 2", "body": "Some useful information" },
-   { "id": "3", "name": "A Guide 3", "body": "Some useful information" }
+   { "id": "1", "name": "A Guide 1", "body": "Some useful information 1" },
+   { "id": "2", "name": "A Guide 2", "body": "Some useful information 2" },
+   { "id": "3", "name": "A Guide 3", "body": "Some useful information 3" }
 ]
