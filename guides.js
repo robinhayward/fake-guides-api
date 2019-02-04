d0a88f3e019e0cad8d56ddc92306b4fb6efa0140
Robin Hayward <mayker.invoice@gmail.com>
2019-02-04 15:04:44 +0000

    Update guides.js

--- guides.js
+++ guides.js
@@ -1,7 +1,5 @@
-{
-  { "data": [
-    { id: 1, title: "A Guide 1", body: "Some useful information" },
-    { id: 2, title: "A Guide 2", body: "Some useful information" },
-    { id: 3, title: "A Guide 3", body: "Some useful information" }
-  ]
-}
+[
+   { id: 1, title: "A Guide 1", body: "Some useful information" },
+   { id: 2, title: "A Guide 2", body: "Some useful information" },
+   { id: 3, title: "A Guide 3", body: "Some useful information" }
+]
