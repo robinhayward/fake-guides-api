fb774297454c5e44f67cf167a76b7586ace0eba8
Robin Hayward <mayker.invoice@gmail.com>
2019-02-04 15:54:17 +0000

    Update guides.js

--- guides.js
+++ guides.js
@@ -1,5 +1,5 @@
 [
-   { "uuid": "1", "version": "1.0.0", "name": "A Guide 1", "body": "Some useful information 1" },
-   { "uuid": "2", "version": "1.0.0", "name": "A Guide 2", "body": "Some useful information 2" },
-   { "uuid": "3", "version": "1.0.0", "name": "A Guide 3", "body": "Some useful information 3" }
+   { "uuid": "1", "version": "1.0.0", "updated_at": "2019-01-02T00:13:22.123Z", "name": "A Guide 1", "body": "Some useful information 1" },
+   { "uuid": "2", "version": "1.0.0", "updated_at": "2019-01-02T00:13:22.123Z", "name": "A Guide 2", "body": "Some useful information 2" },
+   { "uuid": "3", "version": "1.0.0", "updated_at": "2019-01-02T00:13:22.123Z", "name": "A Guide 3", "body": "Some useful information 3" }
 ]
